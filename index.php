c7caaa5bf20097d132c0147dc8cd4498d9ccd6d7
Lenin-Martinez <88004087+Lenin-Martinez@users.noreply.github.com>
2022-03-15 13:55:12 -0600

    Base del proyecto

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/estiloIndex.css">
+    <title>Inicio</title>
+</head>
+
+<header>
+
+    <div class="Logo">
+        <img src="Imagenes/Index/Tigre2.png" alt="error al cargar el logo" width="150">
+    </div>
+    <div class="Textos">
+        <div class="Nombre">
+            Nombre de la veterinaria.
+        </div>
+        <div class="Eslogan">
+            Una frase o eslogan interesante para la pagina, algo que llame la atencion.
+        </div>
+    </div>
+
+    <div class="Iniciar">
+        <img src="Imagenes/Index/IniciarSesionf.png" alt="error al cargar inicio de sesion" width="50">
+        <label>Iniciar sesión</label>
+    </div>
+
+    <div class="Registrar">
+        <img src="Imagenes/Index/Registratef.png" alt="error al cargar inicio de sesion" width="50">
+        <label>Registrate</label>
+    </div>
+
+    <div class="Informacion">
+        <img src="Imagenes/Index/Infof.png" alt="error al cargar inicio de sesion" width="50">
+        <label>Información</label>
+    </div>
+
+</header>
+
+
+<body>
+    <div class="BarraMenu">
+        <a href="index.php" class="OpcionBarra">Inicio</a>
+        <a href="Servicios.php" class="OpcionBarra">Servicios</a>
+        <a href="Productos.php" class="OpcionBarra">Productos</a>
+    </div><br>
+</body>
+
+
+<footer>
+    <div class="Copyright">
+        copyright (C) ...
+    </div>
+    
+    <div class="Contacto">
+        <img src="Imagenes/Index/Contactanosf.png" alt="error al cargar inicio de sesion" width="50">
+        <label>Contactanos</label>
+    </div>
+</footer>
+</html>
