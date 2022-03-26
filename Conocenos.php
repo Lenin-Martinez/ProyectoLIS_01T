35e7fcbff33286e1d98b6b84dc4c0a62446e27e5
Eame-prog <88166948+Eame-prog@users.noreply.github.com>
2022-03-25 22:19:55 -0600

    Add files via upload

---
+++ Conocenos.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/estiloIndex.css">
+    <title>Pet Rescue - Conocenos</title>
+</head>
+
+<header>
+
+    <div class="Logo">
+        <img src="Imagenes/Index/royal.png" alt="error al cargar el logo" width="100%">
+    </div>
+    <div class="Textos">
+        <div class="Nombre">
+            <img src="Imagenes/Index/indextop.png" alt="error al cargar imagen" width="100%">
+        </div>
+    </div>
+
+    <div class="Iniciar">
+        <img src="Imagenes/Index/IniciarSesionf.png" alt="error al cargar inicio de sesion" width="50">
+        <label>Iniciar sesión</label>
+    </div>
+
+    <div class="Registrar">
+        <img src="Imagenes/Index/Registratef.png" alt="error al cargar inicio de sesion" width="50">
+        <label>Registrate</label>
+    </div>
+
+    <div class="Informacion">
+        <img src="Imagenes/Index/Infof.png" alt="error al cargar inicio de sesion" width="50">
+        <label>Información</label>
+    </div>
+
+</header>
+
+
+<body>
+    <div class="BarraMenu">
+        <a href="index.php" class="OpcionBarra">Inicio</a>
+        <a href="Servicios.php" class="OpcionBarra">Servicios</a>
+        <a href="Productos.php" class="OpcionBarra">Productos</a>
+        <a href="Conocenos.php" class="OpcionBarra">Conocenos</a>
+        <a href="contactanos.php" class="OpcionBarra">Contactanos</a>
+    </div><br>
+
+    <p class="indextitle">MISION</p>
+    <p class="indexdesc">Brindar un servicio especializado y de la mejor calidad en el area de servicio veterinario y pet grooming superando las expectativas de nuestros clientes y los pequeños peludos. Llevando a cabo nuestro labor promoviendo el respeto y el cuidado hacia los animales.</p>
+    <p class="indextitle">VISION</p>
+    <p class="indexdesc">Ser el Centro Veterinario de atención Clínica y estética mas reconocido a nivel nacional por su labor y desempeño en el area de cuidado animal, brindado un servicio completo y de alta calidad; para llegar a ser una referencia de calidad a nivel internacional y nacional.</p>
+
+    <p class="indextitle">NUESTROS SERVICIOS</p>
+    <p class="indexdesc">Consulta, Vacunas, Profilaxis Dental, Desparasitaciones, Cirugias, Pet Shop, Farmacia, AccesorioS, Concentrados y Grooming</p>
+
+           <div>
+        <table>
+            <tr class="Servicios">
+                <td class="Servicios"><img src="Imagenes/Conocenos/sv1.png" alt="error al cargar imagen" width=100></td>
+                <td class="Servicios"><img src="Imagenes/Conocenos/sv2.png" alt="error al cargar imagen" width=100></td>
+                <td class="Servicios"><img src="Imagenes/Conocenos/sv3.png" alt="error al cargar imagen" width=100></td>
+                <td class="Servicios"><img src="Imagenes/Conocenos/sv4.png" alt="error al cargar imagen" width=100></td>
+            </tr>
+        </table>
+    </div>
+
+
+</body>
+<footer>
+    <div class="BarraMenu"><p>PET RESCUE</p></div>
+    <div class="Copyright">
+        Copyright (C) Todos los derechos reservados.
+    </div>
+    
+    <div class="Contacto">
+        <img src="Imagenes/Index/Contactanosf.png" alt="error al cargar inicio de sesion" width="50">
+        <label>Contactanos</label>
+    </div>
+</footer>
+</html>
