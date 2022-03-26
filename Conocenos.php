9e60fe6dd84bafc9cdbee9090358c7aec689f6a0
Eame-prog <88166948+Eame-prog@users.noreply.github.com>
2022-03-26 17:12:05 -0600

    Update Conocenos.php

--- Conocenos.php
+++ Conocenos.php
@@ -70,7 +70,7 @@
 <footer>
     <div class="BarraMenu"><p>PET RESCUE</p></div>
     <div class="Copyright">
-        Copyright (C) Todos los derechos reservados.
+        <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br />Esta obra está bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Licencia Creative Commons Atribución-NoComercial-CompartirIgual 4.0 Internacional</a>.
     </div>
     
         <div class="Contacto">
