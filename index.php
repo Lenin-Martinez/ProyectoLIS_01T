6d59a49c3c28a847a776546ec7c04a414354035c
Lenin-Martinez <88004087+Lenin-Martinez@users.noreply.github.com>
2022-03-26 11:42:43 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/estiloIndex.css">
+    <title>Inicio</title>
+</head>
+
+<header>
+    <div class="Logo">
+        <img src="Imagenes/Index/royal.png" alt="error al cargar el logo" width="100%">
+    </div>
+    <div class="Textos">
+        <div class="Nombre">
+            <img src="Imagenes/Index/indextop.png" alt="error al cargar imagen" width="100%">
+        </div>
+    </div>
+
+    <div class="Iniciar">
+        <a href="Login.php" class="OpcionBarra"><img src="Imagenes/Index/IniciarSesionf.png" alt="error al cargar inicio de sesion" width="50"></a>
+        <label>Iniciar sesión</label>
+    </div>
+
+    <div class="Registrar">
+        <a href="registrarse.php" class="OpcionBarra"><img src="Imagenes/Index/Registratef.png" alt="error al cargar inicio de sesion" width="50"></a>
+        <label>Registrate</label>
+    </div>
+
+    <div class="Informacion">
+        <img src="Imagenes/Index/Infof.png" alt="error al cargar inicio de sesion" width="50">
+        <label>Información</label>
+    </div>
+
+</header>
+
+
+<body>
+    <div class="BarraMenu">
+        <a href="index.php" class="OpcionBarra">Inicio</a>
+        <a href="Servicios.php" class="OpcionBarra">Servicios</a>
+        <a href="Productos.php" class="OpcionBarra">Productos</a>
+        <a href="Conocenos.php" class="OpcionBarra">Conocenos</a>
+        <a href="contactanos.php" class="OpcionBarra">Contactanos</a>
+    </div><br>
+
+    <div>
+                <h1 class="indextitle">PET RESCUE</h1>
+                <p class="indexdesc">Tu veterinaria de confianza desde 1998. Contamos con servicios veterinarios, pet grooming y pet shop. Especializados en el cuidado felino, canino y de reptiles. Consulta por nuestras promociones de verano, y descuentos con nuestra tarjeta de cliente frecuente "PET LOVERS".</p>
+    </div>
+
+    <p class="indextitle">RESEÑAS DE NUESTROS AMIGUITOS PELUDOS</p>
+
+    <div id="mainindex">
+        <div class="columna4"><img src="Imagenes/Index/post5.jpeg" alt="error al cargar imagen" width=431></div>
+        <div class="columna5"><img src="Imagenes/Index/post7.png" alt="error al cargar imagen" width=466></div>
+        <div class="columna6"><img src="Imagenes/Index/post6.jpg" alt="error al cargar imagen" width=410></div>
+    </div>
+
+    <div>
+        <table class="comentario">
+            <tr>
+                <td><img src="Imagenes/Index/comm1.png" alt="error al cargar imagen" width=225></td>
+                <td width=200>
+                    <p>@Thomas506</p>
+                    <p>Tommy Junior, esta encantado con los servicios de Pet Rescue, sin duda una de las mejores veterinarias, siempre muy amables y serviciales.</p>
+                </td>
+                <td><img src="Imagenes/Index/comm2.png" alt="error al cargar imagen" width=225></td>
+                <td width=200>
+                    <p>@Clint456</p>
+                    <p>Cuentan con excelentes instalanciones, amplio parqueo, y siempre en la pet shop encuentro todo lo que busco, totalmente recomendado.</p>
+                </td>
+                <td><img src="Imagenes/Index/comm3.png" alt="error al cargar imagen" width=225></td>
+                <td width=175>
+                    <p>@REX909</p>
+                    <p>Una veterinaria que atienda las necesidades de los reptiles no se ve todos los dias, totalmente equipado y capacitado.</p>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <p class="indextitle">CONOCE NUESTRAS PROMOCIONES ESPECIALES PARA TI</p>
+    <div id="mainindex">
+        <div class="columna7"><img src="Imagenes/Index/post1.png" alt="error al cargar imagen" width="250"></div>
+        <div class="columna7"><img src="Imagenes/Index/post8.png" alt="error al cargar imagen" width="250"></div>
+        <div class="columna7"><img src="Imagenes/Index/post9.png" alt="error al cargar imagen" width="250"></div>
+        <div class="columna8"><img src="Imagenes/Index/post10.png" alt="error al cargar imagen" width="250"></div>
+
+    </div>
+</body>
+
+
+
+<footer>
+    <div class="BarraMenu"><p>PET RESCUE</p></div>
+    <div class="Copyright">
+        Copyright (C) Todos los derechos reservados.
+    </div>
+    
+    <div class="Contacto">
+        <a href="contactanos.php" class="OpcionBarra"><img src="Imagenes/Index/Contactanosf.png" alt="error al cargar imagen" width="50"></a>        
+        <label>Contactanos</label>
+    </div>
+</footer>
+</html>
