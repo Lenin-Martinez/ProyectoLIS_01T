0f92d613ea50f3492dd26f103f803c8ed16a2ed8
Lenin-Martinez <88004087+Lenin-Martinez@users.noreply.github.com>
2022-05-08 23:45:47 -0600

    Update login.php

--- login.php
+++ login.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/estiloIndex.css">
     <link rel="stylesheet" href="CSS/sesion.css">
-    <title>Inicio</title>
+    <title>Pet Rescue</title>
 </head>
-
+<form method="POST">
 <header>
     <div class="Logo">
         <img src="Imagenes/Index/royal.png" alt="error al cargar el logo" width="100%">
@@ -19,33 +19,84 @@
             <img src="Imagenes/Index/indextop.png" alt="error al cargar imagen" width="100%">
         </div>
     </div>
-    <div class="Iniciar">
-        <a href="Login.php" class="OpcionBarra"><img src="Imagenes/Index/IniciarSesionf.png" alt="error al cargar inicio de sesion" width="50"></a>
-        <label>Iniciar sesión</label>
-    </div>
+    <?php
+        $basedatos = "pets";
+        $conn = new mysqli('127.0.0.1', 'root', '',$basedatos);
+        $sesion = "SELECT COUNT(*) AS total FROM usuarios WHERE estado='activo';";
+        $Check = mysqli_query($conn,$sesion); 
+        $row = mysqli_fetch_assoc($Check);   
+        if($row['total'] == '0'){
+            echo '<div class="Iniciar">
+            <a href="Login.php" class="OpcionBarra"><img src="Imagenes/Index/IniciarSesionf.png" alt="error al cargar inicio de sesion" width="50"></a>
+            <label>Iniciar sesión</label>
+            </div>
+            
+            <div class="Registrar">
+            <a href="registrarse.php" class="OpcionBarra"><img src="Imagenes/Index/Registratef.png" alt="error al cargar inicio de sesion" width="50"></a>
+            <label>Registrate</label>
+            </div>
+            
+            <div class="Informacion">
+            <a href="Conocenos.php"><img src="Imagenes/Index/Infof.png" alt="error al cargar inicio de sesion" width="50">
+            <label class="OpcionBarra">Información</label>
+            </div>';
 
-    <div class="Registrar">
-        <a href="registrarse.php" class="OpcionBarra"><img src="Imagenes/Index/Registratef.png" alt="error al cargar inicio de sesion" width="50"></a>
-        <label>Registrate</label>
-    </div>
+        }else{
+            echo '<div class="Iniciar">
+            <a href="registrarMascota.php" class="OpcionBarra"><img src="Imagenes/Index/servicios.png" alt="error al cargar inicio de sesion" width="50"></a>
+            <label>Servicios</label>
+            </div>
+            
+            <div class="Registrar">
+            <a href="registrarse.php" class="OpcionBarra"><img src="Imagenes/Index/historial.png" alt="error al cargar inicio de sesion" width="50"></a>
+            <label>Historial</label>
+            </div>
 
-    <div class="Informacion">
-        <img src="Imagenes/Index/Infof.png" alt="error al cargar inicio de sesion" width="50">
-        <label>Información</label>
-    </div>
+            <div class="Informacion">
+            <a href="cerrars.php" class="OpcionBarra">
+            <img src="Imagenes/Index/CerrarSesion.png"alt="error al cargar inicio de sesion" width="50"></a>
+            <label>Cerrar sesión</label>
+            </div>';
+        }
+?>
 
 </header>
 
 <body>
-    <div class="BarraMenu">
+<div class="BarraMenu">
         <a href="index.php" class="OpcionBarra">Inicio</a>
-        <a href="Servicios.php" class="OpcionBarra">Servicios</a>
         <a href="Productos.php" class="OpcionBarra">Productos</a>
         <a href="Conocenos.php" class="OpcionBarra">Conocenos</a>
         <a href="contactanos.php" class="OpcionBarra">Contactanos</a>
     </div><br>
 
-<h2 class="indextitle">Inicio de Sesión</h2>
+    <?PHP
+    if(isset($_POST["btn1"]))
+    {
+        if((!empty($_POST["nombre"])) && (!empty($_POST["contra"])))
+        {
+            $nombre=$_POST["nombre"];
+            $contra=$_POST["contra"];
+            $basedatos = "pets";
+            $conn = new mysqli('127.0.0.1', 'root', '',$basedatos);
+            $consult1="SELECT COUNT(*) AS total FROM usuarios WHERE usuario='$nombre' AND contrasenia='$contra';";
+            $CheckUser = mysqli_query($conn,$consult1); 
+            $row = mysqli_fetch_assoc($CheckUser);   
+            if($row['total'] == '0'){
+                echo '<p class="camponame">Usuario o contraseña incorrecta. Intente de nuevo.</p>';
+            }else{
+                $consult2 ="UPDATE usuarios SET estado='activo' WHERE usuario='$nombre';";
+                $Eject2 = mysqli_query($conn,$consult2); 
+                header('Location: http://localhost/xampp/ProyectoLIS_01T/registrarMascota.php');
+            }
+        }
+        else
+        {
+        }
+    }
+    ?>
+
+    <h2 class="indextitle">Inicio de Sesión</h2>
     <div class="section1">
         <form method="POST">
             <label>
@@ -57,12 +108,12 @@
                 <input type="text" id="contra" name="contra" class="campo" placeholder="Ingrese aqui su contraseña" required>
             </label>
             <div>
-                <button type="submit" class="boton1" name="IniciarS">Iniciar Sesión</button>
+                <button type="submit" class="boton1" name="btn1" id="btn1">Iniciar Sesión</button>
             </div>
         </div><br>
         <div class="acceder">
             <div  class="g-signin2" data-onsuccess="onSignIn"></div><br>
-    </div>
+    </div>';
 </form>
 </body>
 
@@ -86,43 +137,3 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-
-
-<?php
-if(isset($_POST["IniciarS"]))
-{
-    if((!empty($_POST["nombre"])) && (!empty($_POST["contra"])))
-    {
-        $Nombre=$_POST["nombre"];
-        $Contrasenia=$_POST["contra"];
-
-        IniciarS($Nombre, $Contrasenia);
-    }
-}
-
-
-
-
-function IniciarS($Nombre, $Contrasenia)
-{
-    $url= "https://pet-rescue-57447-default-rtdb.firebaseio.com/Usuarios.json";
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    
-    $DatosObtenidos = curl_exec($ch);
-    curl_close($ch);
-
-    $Resultados = json_decode($DatosObtenidos,true);
-
-    //Es el numero del indice
-    $a = array_search(array_column($Nombre), array_column($Resultados,'Nombre'));
-    $b = array_search($Contrasenia, array_column($Resultados,'Contrasenia'));
-
-    if($a == $b)
-    {
-        header("Location: Servicios.php");
-    }
-}
-?>
